b157be96e17956f425805471e04fb8fa92f2866d
nattsmg <natalia.semiguem.machado@escola.pr.gov.br>
2026-04-01 15:20:58 +0000

    conversoes

---
+++ conversoes.js
@@ -0,0 +1,9 @@
+// tipo de dados
+// booleanos
+
+// conversoes implicitas
+
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero === numeroString);
--- falsoverdadeiro.js
+++ falsoverdadeiro.js
@@ -1,7 +1,24 @@
-const usuariologado = true;
-const contapaga = false;
+//const usuariologado = true;
+//const contapaga = false;
 
 //0 ==> false
 //1 ==> true
 
-console.log (0 == false);
+//console.log (0 == false);
+//console.log (1 == true);
+//console.log ("" == false);
+
+//null  
+//undefined
+
+let minhaVar;
+let varNull = null;
+
+console.log (minhaVar)
+console.log(varNull)
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero)
+console.log(typeof texto)
